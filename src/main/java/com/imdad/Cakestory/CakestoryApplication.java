6a8109783cb0d2b26858e538b655ae224a5eaa2f
Md.Imdadul Haque <imdadhaque2210@gmail.com>
2024-08-20 18:12:34 +0600

    Final Production v.1

---
+++ src/main/java/com/imdad/Cakestory/CakestoryApplication.java
@@ -0,0 +1,13 @@
+package com.imdad.Cakestory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakestoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakestoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imdad/Cakestory/CakestoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imdad.Cakestory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakestoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
